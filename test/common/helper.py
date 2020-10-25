bee8497aad093afa81449e4dd190dd2abc3788cd
donyahojabr <donya.hojabr@mail.mcgill.ca>
2020-10-25 16:02:39 -0400

    categories_id_projects_id

--- test/common/helper.py
+++ test/common/helper.py
@@ -3,6 +3,7 @@ import json
 
 base_url = 'http://localhost:4567/'
 
+
 def reset_system():
     res = requests.post(base_url + 'admin/data/thingifier')
     if res.status_code == 200:
@@ -10,27 +11,48 @@ def reset_system():
     else:
         raise Exception("System failed to reset")
 
+
 def create_todo(body, headers={'Content-Type': 'application/json'}):
-    res = requests.post(base_url + 'todos', headers=headers, data=json.dumps(body))
+    res = requests.post(base_url + 'todos', headers=headers,
+                        data=json.dumps(body))
     return res.json()
+
 
 def create_category(body, headers={'Content-Type': 'application/json'}):
-    res = requests.post(base_url + 'categories', headers=headers, data=json.dumps(body))
+    res = requests.post(base_url + 'categories',
+                        headers=headers, data=json.dumps(body))
     return res.json()
+
 
 def create_project(body, headers={'Content-Type': 'application/json'}):
-    res = requests.post(base_url + 'projects', headers=headers, data=json.dumps(body))
+    res = requests.post(base_url + 'projects',
+                        headers=headers, data=json.dumps(body))
     return res.json()
 
+
 def create_todo_category_relation(todo_id, category_id):
-    headers={'Content-Type': 'application/json'}
-    requests.post(base_url + 'todos/' + str(todo_id) + '/categories', headers=headers, data=json.dumps({ 'id': category_id }))
-    requests.post(base_url + 'categories/' + str(category_id) + '/todos', headers=headers, data=json.dumps({ 'id': todo_id }))
+    headers = {'Content-Type': 'application/json'}
+    requests.post(base_url + 'todos/' + str(todo_id) + '/categories',
+                  headers=headers, data=json.dumps({'id': category_id}))
+    requests.post(base_url + 'categories/' + str(category_id) +
+                  '/todos', headers=headers, data=json.dumps({'id': todo_id}))
+
 
 def create_todo_project_relation(todo_id, project_id):
-    headers={'Content-Type': 'application/json'}
-    requests.post(base_url + 'todos/' + str(todo_id) + '/tasksof', headers=headers, data=json.dumps({ 'id': project_id }))
-    requests.post(base_url + 'projects/' + str(project_id) + '/tasks', headers=headers, data=json.dumps({ 'id': todo_id }))
+    headers = {'Content-Type': 'application/json'}
+    requests.post(base_url + 'todos/' + str(todo_id) + '/tasksof',
+                  headers=headers, data=json.dumps({'id': project_id}))
+    requests.post(base_url + 'projects/' + str(project_id) +
+                  '/tasks', headers=headers, data=json.dumps({'id': todo_id}))
+
+
+def create_category_project_relation(category_id, project_id):
+    headers = {'Content-Type': 'application/json'}
+    requests.post(base_url + 'categories/' + str(category_id) +
+                  '/projects', headers=headers, data=json.dumps({'id': project_id}))
+    requests.post(base_url + 'projects/' + str(project_id) + '/categories',
+                  headers=headers, data=json.dumps({'id': category_id}))
+
 
 def print_response(response):
     print('Status Code:', response.status_code)
---
+++ test/test_categories/test_categories_id_projects_id.py
@@ -0,0 +1,305 @@
+import pytest
+import requests
+import json
+from test.common.helper import reset_system, create_category, create_project, print_response, create_category_project_relation
+import xml.dom.minidom
+
+base_url = 'http://localhost:4567/categories/'
+project_url = 'http://localhost:4567/projects/'
+
+
+def url(category_id, project_id):
+    return base_url + str(category_id) + '/projects/' + str(project_id)
+
+
+def setup_function(function):
+    reset_system()
+
+
+def teardown_function(function):
+    pass
+
+
+def test_get_category_project_not_allowed():
+
+    # Given
+    headers = {'Content-Type': 'application/json'}
+
+    project = {
+        'title': 'Office Work',
+        'completed': False,
+        'active': False,
+        'description': 'a description'
+    }
+
+    project_id = create_project(project)['id']
+
+    category = {
+        'title': 'Category title 1',
+        'description': 'this is a description',
+        'projects': [
+            {
+                'id': project_id
+            }
+        ]
+    }
+
+    category_id = create_category(category)['id']
+
+    # When
+    res = requests.get(url(category_id, project_id), headers=headers)
+
+    # Then
+    print_response(res)
+
+    assert res.status_code == 405
+
+
+def test_put_category_project_not_allowed():
+
+    # Given
+    headers = {'Content-Type': 'application/json'}
+
+    project = {
+        'title': 'Office Work',
+        'completed': False,
+        'active': False,
+        'description': 'a description'
+    }
+
+    project_id = create_project(project)['id']
+
+    category = {
+        'title': 'Category title 1',
+        'description': 'this is a description',
+        'projects': [
+            {
+                'id': project_id
+            }
+        ]
+    }
+
+    category_id = create_category(category)['id']
+
+    # When
+    res = requests.put(url(category_id, project_id), headers=headers)
+
+    # Then
+    print_response(res)
+
+    assert res.status_code == 405
+
+
+def test_post_category_project_not_allowed():
+
+    # Given
+    headers = {'Content-Type': 'application/json'}
+
+    project = {
+        'title': 'Office Work',
+        'completed': False,
+        'active': False,
+        'description': 'a description'
+    }
+
+    project_id = create_project(project)['id']
+
+    category = {
+        'title': 'Category title 1',
+        'description': 'this is a description',
+        'projects': [
+            {
+                'id': project_id
+            }
+        ]
+    }
+
+    category_id = create_category(category)['id']
+
+    # When
+    res = requests.post(url(category_id, project_id), headers=headers)
+
+    # Then
+    print_response(res)
+
+    assert res.status_code == 405
+
+
+def test_delete_category_project():
+
+    # Given
+    headers = {'Content-Type': 'application/json'}
+
+    project = {
+        'title': 'Office Work',
+        'completed': False,
+        'active': False,
+        'description': 'a description'
+    }
+
+    project_id = create_project(project)['id']
+
+    category = {
+        'title': 'Category title 1',
+        'description': 'this is a description',
+        'projects': [
+            {
+                'id': project_id
+            }
+        ]
+    }
+
+    category_id = create_category(category)['id']
+
+    create_category_project_relation(category_id, project_id)
+
+    # When
+    res = requests.delete(url(category_id, project_id), headers=headers)
+
+    # Then
+    print_response(res)
+
+    assert res.status_code == 200
+
+    # Fetch category and project to assert that the project relationship was deleted
+    updated_category = requests.get(base_url + category_id, headers=headers)
+    updated_category_body = updated_category.json()
+
+    print('Category:')
+    print_response(updated_category)
+
+    assert updated_category_body['categories'][0].get('projects') is None
+
+    updated_project = requests.get(project_url + category_id, headers=headers)
+    updated_project_body = updated_project.json()
+
+    print('Project:')
+    print_response(updated_project)
+
+    assert updated_project_body['projects'][0].get('categories') is None
+
+
+def test_delete_category_project_relationship_does_not_exist():
+
+    # Given
+    headers = {'Content-Type': 'application/json'}
+
+    project = {
+        'title': 'Office Work',
+        'completed': False,
+        'active': False,
+        'description': 'a description'
+    }
+
+    project_id = create_project(project)['id']
+
+    category = {
+        'title': 'Category title 1',
+        'description': 'this is a description'
+    }
+
+    category_id = create_category(category)['id']
+
+    # When
+    res = requests.delete(url(category_id, project_id), headers=headers)
+
+    # Then
+    print_response(res)
+
+    assert res.status_code == 404
+
+
+def test_delete_category_project_project_does_not_exist():
+
+    # Given
+    headers = {'Content-Type': 'application/json'}
+
+    project_id = '999'
+
+    category = {
+        'title': 'Category title 1',
+        'description': 'this is a description'
+    }
+
+    category_id = create_category(category)['id']
+
+    # When
+    res = requests.delete(url(category_id, project_id), headers=headers)
+
+    # Then
+    print_response(res)
+
+    assert res.status_code == 404
+
+
+def test_delete_category_project_category_does_not_exist():
+
+    # Given
+    headers = {'Content-Type': 'application/json'}
+
+    project = {
+        'title': 'Office Work',
+        'completed': False,
+        'active': False,
+        'description': 'a description'
+    }
+
+    project_id = create_project(project)['id']
+
+    category_id = '999'
+
+    # When
+    res = requests.delete(url(category_id, project_id), headers=headers)
+
+    # Then
+    print_response(res)
+
+    assert res.status_code == 404
+
+
+def test_options_category_project_ok():
+
+    # Given
+    headers = {'Content-Type': 'application/json'}
+
+    any_id = '999'
+
+    # When
+    res = requests.options(url(any_id, any_id), headers=headers)
+
+    # Then
+    print_response(res)
+
+    assert res.status_code == 200
+
+
+def test_head_category_project_not_allowed():
+
+    # Given
+    headers = {'Content-Type': 'application/json'}
+
+    any_id = '999'
+
+    # When
+    res = requests.head(url(any_id, any_id), headers=headers)
+
+    # Then
+    print_response(res)
+
+    assert res.status_code == 405
+
+
+def test_patch_category_project_not_allowed():
+
+    # Given
+    headers = {'Content-Type': 'application/json'}
+
+    any_id = '999'
+
+    # When
+    res = requests.patch(url(any_id, any_id), headers=headers)
+
+    # Then
+    print_response(res)
+
+    assert res.status_code == 405
