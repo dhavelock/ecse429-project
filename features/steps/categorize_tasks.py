866c96ff5f443e6509e30886d3f6f3a0fba1050d
donyahojabr <donya.hojabr@mail.mcgill.ca>
2020-11-09 09:10:03 -0500

    small refactor

--- features/steps/categorize_tasks.py
+++ features/steps/categorize_tasks.py
@@ -4,51 +4,51 @@ from test.common.helper import create_project, create_category, delete_project, 
 
 @given('a todo task exists in the system with title "{todoTitle}"')
 def step_impl(context, todoTitle):
-    context.todo_id = create_todo({'title': todoTitle})['id']
+    context.todo = create_todo({'title': todoTitle})
 
 
 @given('a "{priorityLevel}" priority category exists in the system')
 def step_impl(context, priorityLevel):
-    context.category_id = create_category({'title': priorityLevel})['id']
+    context.category = create_category({'title': priorityLevel})
 
 
 @given('a category exists in the system with title "{categoryTitle}"')
 def step_impl(context, categoryTitle):
-    context.category_id_1 = create_category({'title': categoryTitle})['id']
+    context.category_1 = create_category({'title': categoryTitle})
 
 
 @given('a todo task exists with title "{todoTitle}" and category with title "{categoryTitle}"')
 def step_impl(context, todoTitle, categoryTitle):
-    context.todo_id = create_todo({'title': todoTitle})['id']
+    context.todo = create_todo({'title': todoTitle})
     create_todo_category_relation(
-        context.todo_id, context.category_id_1)
+        context.todo['id'], context.category_1['id'])
 
 
 @when('I categorize the task as "{priorityLevel}" priority')
 def step_impl(context, priorityLevel):
-    create_todo_category_relation(context.todo_id, context.category_id)
+    create_todo_category_relation(context.todo['id'], context.category['id'])
 
 
 @when('I request to add a "{priorityLevel}" priority cartegorization to the task using an invalid id')
 def step_impl(context, priorityLevel):
-    create_todo_category_relation(context.todo_id, '999')
+    create_todo_category_relation(context.todo['id'], '999')
 
 
 @then('the todo task with title "{todoTitle}" should have the "{priorityLevel}" priority categorization')
 def step_impl(context, todoTitle, priorityLevel):
-    response = get_todo(context.todo_id)
+    response = get_todo(context.todo['id'])
     assert response.json()[
-        'todos'][0]['categories'][0]['id'] == context.category_id
+        'todos'][0]['categories'][0]['id'] == context.category['id']
 
 
 @then('the todo task with title "{todoTitle}" should have both categories associated with it')
 def step_impl(context, todoTitle):
-    response = get_todo(context.todo_id)
+    response = get_todo(context.todo['id'])
     assert len(response.json()[
         'todos'][0]['categories']) == 2
 
 
 @then('the todo task should not have any category relations')
 def step_impl(context):
-    response = get_todo(context.todo_id)
+    response = get_todo(context.todo['id'])
     assert 'categories' not in response.json()['todos'][0]
