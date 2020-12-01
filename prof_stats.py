15e38d96e266b0cd6ee55fdbf64c5c0209cfea6c
Dylan Havelock <dylan.havelock@gmail.com>
2020-12-01 13:53:16 -0500

    updated pstats regex

--- prof_stats.py
+++ prof_stats.py
@@ -1,3 +1,3 @@
 import pstats
 p = pstats.Stats('prof/combined.prof')
-p.strip_dirs().sort_stats('cumulative').print_stats(r'^test_.*(?<!\(setup_function\))$')
+p.strip_dirs().sort_stats('cumulative').print_stats(r'^test_.*(\(test_)')
