cecc9b899d7eb19b6911a3e5a2cee880ffab886e
Dylan Havelock <dylan.havelock@gmail.com>
2020-12-02 21:32:51 -0500

    performance tests

---
+++ perf_test.py
@@ -0,0 +1,15 @@
+import psutil
+import subprocess
+import time
+import datetime
+
+f = open("perf_test_output.csv", "w+")
+p = subprocess.Popen(["pytest","performance"])
+
+f.write('time,cpu %,availble mem. %\n')
+while p.poll() is None:
+    f.write(str(datetime.datetime.now().isoformat()) + ',' + str(psutil.cpu_percent()) + ',' + str(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total) + '\n')
+    time.sleep(1)
+
+f.close()
+print('Performance tests complete')
